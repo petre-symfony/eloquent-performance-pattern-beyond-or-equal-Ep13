825aba83697873d1ea43ddc731e57c11a198b79b
petrero <petrero@gmail.com>
2023-03-10 13:13:19 +0200

    16.4 Now let's take a look at the subquery approach. Instead of using a join to get the company data we use a subquery.
     If we refresh the page we can see that it's still working. However if we look in laravel debug bar we can see that this approach isn't nearly as fast as the join approach running in about 137 ms.
     So when ordering a haveOne relationship definetly rich for the join approach first

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,9 +28,12 @@ class UsersController extends Controller {
             ->paginate();
          */
 
-        $users = User::select('users.*')
-            ->join('companies', 'companies.id', '=', 'users.company_id')
-            ->orderBy('companies.name')
+        $users = User::orderBy(
+                Company::select('name')
+                    ->whereColumn('id', 'users.company_id')
+                    ->orderBy('name')
+                    ->take(1)
+            )
             ->with('company')
             ->paginate();
 
