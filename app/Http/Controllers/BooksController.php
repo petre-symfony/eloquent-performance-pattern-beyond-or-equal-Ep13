c0013da5df8c33217e2669bc542b219c1bf2e544
petrero <petrero@gmail.com>
2023-03-12 12:10:22 +0200

    18.6 To this point we haven't really been ordering by a belongsToMany relationship column. As I already noted checkouts is a really hasMany relationship to our books. So let's try to order by the name of the user who last checked out the book instead.  You can use a subquery to accomplish that.  This time we need to include a join in our subquery to polling the checkouts table. We're doing this because the checkouts table connects our books with our users. We're ordering the books by the name of the user who last checked out that book.  If you deal with a tone of data this approach will not be likely super fast.

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -18,13 +18,13 @@ use App\Models\Customer;
 class BooksController extends Controller {
     public function index() {
         $books = Book::
-            orderByDesc(function ($query) {
-                $query->select('borrowed_date')
-                    ->from('checkouts')
-                    ->whereColumn('book_id', 'books.id')
-                    ->latest('borrowed_date')
-                    ->take(1);
-            })
+            orderBy(User::select('name')
+                ->join('checkouts', 'checkouts.user_id', '=', 'users.id')
+                ->whereColumn('checkouts.book_id', 'books.id')
+                ->latest('checkouts.borrowed_date')
+                ->take(1)
+
+            )
             ->withLastCheckout()
             ->with('lastCheckout.user')
             ->paginate();
