ab13238f55215eaab332ca1579f262b48b9f8f87
petrero <petrero@gmail.com>
2023-03-13 20:04:46 +0200

    19.8 Postgress, on the other hand, has built-in support for ordering with null always last.  If we refresh the browser we can see that it's still working the way we wanted. If we go to the last page we can see that the null values are last. And if we sort descending again it's still working.  Let's take a peek at the query in the laravel debug bar:   SELECT * FROM users ORDER BY town DESC nulls last and then ordering by their name ascending. Very cool. Postgress makes that very easy to do.  However we must be careful here. We've just introduced a series of sql injections vulnerability. We're taking the direction from the request and simply inliningdirectly in our query. That's no good. Because someone could teoretically now to change the direction for something else that modify our query. So how could we protect ourseleves from these?

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,9 +39,14 @@ class UsersController extends Controller {
 
         $users = User::
             when(request('sort') === 'town', function ($query){
-                $query
-                    ->orderByRaw('town is null')
-                    ->orderBy('town', request('direction'));
+                if (config('database.default') === 'mysql' || config('database.default') === 'sqlite') {
+                    $query
+                        ->orderByRaw('town is null')
+                        ->orderBy('town', request('direction'));
+                }
+                if (config('database.default') === 'pgsql') {
+                    $query->orderByRaw('town '.request('direction').' nulls last');
+                }
             })
             ->orderBy('name')
             ->paginate();
