69127dcd0769c51fc1279ed336a388431ff0f004
petrero <petrero@gmail.com>
2023-03-14 18:52:16 +0200

    20.6 Create an index for the status custom algorithm. This gonna be a superhelpfull optimization to make if you are using a custom sorting algorithm like this on a large dataset.

--- database/migrations/2023_03_13_195734_create_features_table.php
+++ database/migrations/2023_03_13_195734_create_features_table.php
@@ -14,6 +14,13 @@ return new class extends Migration {
             $table->string('title', 100);
             $table->string('status', 10);
             $table->timestamps();
+            $table->rawIndex('(
+                case
+                    when status = "Requested" then 1
+                    when status = "Approved" then 2
+                    when status = "Completed" then 3
+                end
+            )', 'features_status_ranking_index');
         });
     }
 
