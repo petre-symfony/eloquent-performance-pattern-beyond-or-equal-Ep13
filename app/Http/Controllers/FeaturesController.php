fc5e12a1bc40849f18fc7517671d4cfac378826b
petrero <petrero@gmail.com>
2023-03-14 18:02:56 +0200

    20.3 Let's start by adding a when call to our query. This will check to see if the sort query parameter has been set for this request and if it is it will run whatever the callback we provide. This callback receives both the instance of the query builder and the sort value as its arguments. Next we gonna use the switch statements to check the value of the sort variableso we can run the appropriate orderBy query for it. Next add the case statements for the three columns.  I customize how the status is sorting. We add an only orderByStatus scope for now and we're passing the direction. Order by activity is obviously not as simple. Our activity includes counts for both votes and comments. So we need to find a meaningful way to order those together somehow. Again, let's just add an orderByActivity scope for now passing the direction.  And now if go back into the browser and sort the title ascending we can see that it's working and if we click it again we sorting order descending.

--- app/Http/Controllers/FeaturesController.php
+++ app/Http/Controllers/FeaturesController.php
@@ -13,6 +13,13 @@ use App\Models\Feature;
 class FeaturesController extends Controller {
     public function index() {
         $features = Feature::withCount('comments', 'votes')
+            ->when(request('sort'), function ($query, $sort) {
+                switch($sort) {
+                    case 'title': return $query->orderBy('title', request('direction'));
+                    case 'status': return $query->orderByStatus(request('direction'));
+                    case 'activity': return $query->orderByActivity(request('direction'));
+                }
+            })
             ->latest()
             ->paginate();
 
